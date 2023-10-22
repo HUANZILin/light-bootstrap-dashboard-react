6f3860ee5ac361619a4b30c69898e7ab9055b8de
Karen Lin <karen901208@gmail.com>
2023-10-22 19:48:39 +0800

    Add upload area with ant design

--- src/views/Ad.js
+++ src/views/Ad.js
@@ -12,8 +12,28 @@ import {
   Row,
   Col,
 } from "react-bootstrap";
+import { UploadOutlined } from "@ant-design/icons";
+import { Button as AntButton, message, Upload } from "antd";
 
 function Ad() {
+  const props = {
+    name: "file",
+    action: "https://run.mocky.io/v3/435e224c-44fb-4773-9faf-380c5e6a2188",
+    headers: {
+      authorization: "authorization-text",
+    },
+    onChange(info) {
+      if (info.file.status !== "uploading") {
+        console.log(info.file, info.fileList);
+      }
+      if (info.file.status === "done") {
+        message.success(`${info.file.name} file uploaded successfully`);
+      } else if (info.file.status === "error") {
+        message.error(`${info.file.name} file upload failed.`);
+      }
+    },
+  };
+
   return (
     <>
       <Container fluid>
@@ -24,18 +44,21 @@ function Ad() {
                 <Card.Title as="h4">變更廣告橫幅</Card.Title>
               </Card.Header>
               <Card.Body>
-                <h5>廣告1</h5>
+                <h5>廣告1(預覽)</h5>
                 <div className="card-image">
                   <img
                     alt="..."
                     src={require("assets/img/photo-1431578500526-4d9613015464.jpeg")}
                   ></img>
                 </div>
+                <Upload {...props}>
+                  <AntButton icon={<UploadOutlined />}>上傳新圖片</AntButton>
+                </Upload>
               </Card.Body>
             </Card>
           </Col>
           <Col md="4">
-            <Card className="card-user">
+            <Card>
               <Card.Body>
                 <h5>廣告2</h5>
                 <div className="card-image">
@@ -44,11 +67,14 @@ function Ad() {
                     src={require("assets/img/sidebar-1.jpg")}
                   ></img>
                 </div>
+                <Upload {...props}>
+                  <AntButton icon={<UploadOutlined />}>上傳新圖片</AntButton>
+                </Upload>
               </Card.Body>
             </Card>
           </Col>
           <Col md="8">
-            <Card className="card-user">
+            <Card>
               <Card.Body>
                 <h5>廣告3</h5>
                 <div className="card-image">
@@ -57,6 +83,9 @@ function Ad() {
                     src={require("assets/img/sidebar-2.jpg")}
                   ></img>
                 </div>
+                <Upload {...props}>
+                  <AntButton icon={<UploadOutlined />}>上傳新圖片</AntButton>
+                </Upload>
               </Card.Body>
             </Card>
           </Col>
